fc022edb2719583eb09650338acb1bd82ed5f7ad
Piotr-Zacharski <zacharski.p.h@gmail.com>
2022-05-28 13:44:27 +0200

    Layout change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-
+import hm from '/src/img/hm.png';
+import dnd from '/src/img/dnd.png';
+import todo from '/src/img/todo.png';
+import fetch from '/src/img/fetch.png';
+import weather from '/src/img/weather.png';
 function App() {
   return (
     <div className="min-h-screen flex flex-col text-white">
@@ -14,19 +18,19 @@ function App() {
 
         <div className="grid gap-10 grid-cols-3 grid-rows-3 justify-center mt-15">
           <a href="https://hendimejdi.vercel.app/" target="_blank">
-            <img src="./src/img/hm.png" alt="logo" className=" rounded-full justify-center" /><h2 className="p-2">HendiMejdi</h2></a>
+            <img src={hm} alt="logo" className=" rounded-full justify-center" /><h2 className="p-2">HendiMejdi</h2></a>
        
           <a href="https://dnd-board.vercel.app/" target="_blank">
-            <img src="./src/img/dnd.png" alt="logo" className="justify-center rounded-full" /><h2 className="p-2">Drag'n'Drop Board </h2></a>
+            <img src={dnd} alt="logo" className="justify-center rounded-full" /><h2 className="p-2">Drag'n'Drop Board </h2></a>
       
           <a href="https://tscript-todo.netlify.app" target="_blank">
-            <img src="./src/img/todo.png" alt="logo" className="justify-center rounded-full" /><h2 className="p-2">Typescript - Todo</h2></a>
+            <img src={todo} alt="logo" className="justify-center rounded-full" /><h2 className="p-2">Typescript - Todo</h2></a>
 
           <a href="https://axios-psi.vercel.app/" target="_blank">
-            <img src="./src/img/fetch.png" alt="logo" className="justify-center rounded-full" /><h2 className="p-2">Futurama Api Fetch</h2></a>
+            <img src={fetch} alt="logo" className="justify-center rounded-full" /><h2 className="p-2">Futurama Api Fetch</h2></a>
        
           <a href="https://u-weather.vercel.app/" target="_blank">
-            <img src="./src/img/weather.png" alt="logo" className="justify-center rounded-full" /><h2 className="p-2">Weather App</h2></a>
+            <img src={weather} alt="logo" className="justify-center rounded-full" /><h2 className="p-2">Weather App</h2></a>
           
           <h3 className="text-4xl mt-20">◦◦◦<span className="pl-2">more to come</span></h3>
        
